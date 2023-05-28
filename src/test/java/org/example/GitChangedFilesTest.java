530eaa17d0dfa2fb265f2554748a5d6ca430dbd0
Tcharmtchi <tcharmtchi@hotmail.com>
2023-05-29 00:13:43 +0200

    Testing with @Test Annotation

--- src/test/java/org/example/GitChangedFilesTest.java
+++ src/test/java/org/example/GitChangedFilesTest.java
@@ -1,10 +1,12 @@
 package org.example;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class GitChangedFilesTest {
 
-    @org.junit.jupiter.api.Test
+    @Test
     void mainTest() {
         System.out.println("hiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii");
         assertTrue(true);
