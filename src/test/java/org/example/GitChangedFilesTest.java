58a5c1a14b278209faf28c1919af8b9a56fed9a9
Tcharmtchi <tcharmtchi@hotmail.com>
2023-05-28 23:57:40 +0200

    Test Cases

---
+++ src/test/java/org/example/GitChangedFilesTest.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GitChangedFilesTest {
+
+    @org.junit.jupiter.api.Test
+    void main() {
+        System.out.println("hiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii");
+    }
+}
